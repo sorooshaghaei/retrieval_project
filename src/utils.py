92a914d08b6e401e17ad304e1111b1b5910c99e0
Soroosh <soroosh77aghaei@gmail.com>
2026-02-06 17:55:15 +0100

    The Data Loader(utils.py) implemented

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,31 @@
-def load_data(path):
-    pass
-    # opens the JSON and returns a Pandas DataFrame
+import json
+import pandas as pd
+import os
+
+
+def load_data(data_path):
+
+    # 1. file paths
+    # os.path.join so it works on windows, mac...
+    docs_path = os.path.join(data_path, "docs.json")
+    train_queries_path = os.path.join(data_path, "queries_train.json")
+    test_queries_path = os.path.join(data_path, "queries_test.json")
+    qgts_path = os.path.join(data_path, "qgts_train.json")
+
+    # 2. load docs
+    dataframe_docs = pd.read_json(docs_path)
+
+    # 3. load Queries (Train & Test)
+    print(f"---- loading {train_queries_path}...")
+    train_queries_df = pd.read_json(train_queries_path)
+    print(f"---- loading {test_queries_path}...")
+    test_queries_df = pd.read_json(test_queries_path)
+
+    # 4. load queries ground truths (qgts)
+    print(f"---- loading {qgts_path}...")
+    with open(qgts_path, "r") as f:
+        query_ground_truth_data = json.load(f)
+
+    print("Data loading complete.")
+
+    return dataframe_docs, train_queries_df, test_queries_df, query_ground_truth_data
